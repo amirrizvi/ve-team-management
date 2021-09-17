e0cbd84a57815df4192d081a59e1e265afda260a
Amir <amirrizvi89@gmail.com>
2021-09-17 15:05:34 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import Mongoose from "mongoose"
+
+const connectDB = async () => {
+  try {
+    const conn = await Mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    console.log(`MongoDB Connected:${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error:${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB
